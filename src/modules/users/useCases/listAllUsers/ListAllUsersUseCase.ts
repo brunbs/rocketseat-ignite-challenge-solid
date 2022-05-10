1fe191636b77d30717e03e5c25c67ef1210343b3
brunbs <babosa227@gmail.com>
2022-05-10 17:18:03 -0300

    Challenge implementation

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if(!user) {
+      throw new Error('User not found!');
+    }
+    if(user.admin === true) {
+      return this.usersRepository.list();
+    } else {
+      throw new Error('This user is not allowed to get this list')
+    }
   }
 }
 
